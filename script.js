4cb5b5f381d27fe64b235981e1f437764ae86b1d
fireflame606 <79111380+fireflame606@users.noreply.github.com>
2021-03-19 17:02:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-	if(character.classList != "animate"){
-	character.classList.add("animate");
-	}
-	setTimeout(function(){
-		character.classList.remove("animate");
-	},500);
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
 
 var checkDead = setInterval(function(){
