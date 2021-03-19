d8d37ac7077ae73d69c942ad8cb40807c31dc933
fireflame606 <79111380+fireflame606@users.noreply.github.com>
2021-03-19 16:48:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
+	if(character.classList != "animate"){
 	character.classList.add("animate");
+	}
 	setTimeout(function(){
 		character.classList.remove("animate");
 	},500);
