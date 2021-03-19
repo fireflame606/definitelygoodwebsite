c6806158598570454fd7cbec320e826334650fb2
fireflame606 <79111380+fireflame606@users.noreply.github.com>
2021-03-19 17:25:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function jump(){
 }
 
 var checkDead = setInterval(function(){
-	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-	var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+	let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+	let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 	if(blockLeft<20 && blockLeft>0 && characterTop>=130){
 		block.style.animation="none";
 		block.style.display="none";
 		alert("You lose!");
 	}
-},10);
+}, 10);
